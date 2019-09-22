599fc97c72b488f75422390585b635f36b4001be
Shiyijie66 <497829544@qq.com>
2019-09-22 15:23:33 +0800

    code

---
+++ lib/models/__init__.py
@@ -0,0 +1,13 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.pose_resnet
+import models.pose_hsrnet
+import models.pose_hsr_msnet
